2590bafaccdba50ad4f3b41069cce3453d0b4489
danielb <git@masterunit.net>
2020-07-06 17:44:30 +0200

    v1.0.0

--- cmd/sensu-metric-alert/main.go
+++ cmd/sensu-metric-alert/main.go
@@ -1,15 +1,89 @@
 package main
 
 import (
+	"bufio"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/danielb42/whiteflag"
+)
+
+var (
+	prmEndpoint string
+	prmMetric   string
 )
 
 func main() {
 	log.SetOutput(os.Stderr)
 
-	if len(os.Args) == 1 {
-		println("usage: sensu-metric-alert -m <metric> --lt|--gt|--ne <crit value>")
+	whiteflag.Alias("e", "endpoint", "endpoint to scrape (http://url:port/path)")
+	whiteflag.Alias("m", "metric", "the metric to watch")
+	whiteflag.Alias("l", "lt", "value must be lower than this for crit status")
+	whiteflag.Alias("g", "gt", "value must be greater than this for crit status")
+	whiteflag.Alias("q", "eq", "value must be equal to this for crit status")
+	whiteflag.Alias("n", "ne", "value must be different from this for crit status")
+	whiteflag.ParseCommandLine()
+
+	if !whiteflag.CheckString("e") || !whiteflag.CheckString("m") ||
+		(!whiteflag.CheckInt("l")) && !whiteflag.CheckInt("g") && !whiteflag.CheckInt("q") && !whiteflag.CheckInt("n") {
+		println("usage: sensu-metric-alert -e <endpoint> -m <metric> --lt|--gt|--eq|--ne <crit value>")
 		os.Exit(2)
 	}
+
+	prmEndpoint = whiteflag.GetString("e")
+	prmMetric = whiteflag.GetString("m")
+
+	resp, err := http.Get(prmEndpoint)
+	if err != nil {
+		log.Println("could not scrape metrics from", prmEndpoint, err.Error())
+		os.Exit(2)
+	}
+	defer resp.Body.Close()
+
+	foundMetric := false
+	s := bufio.NewScanner(resp.Body)
+	for s.Scan() {
+		line := strings.Split(s.Text(), " ")
+
+		if strings.HasPrefix(line[0], prmMetric) {
+			foundMetric = true
+			val, err := strconv.ParseFloat(line[1], 64)
+			if err != nil {
+				log.Println(err)
+				os.Exit(2)
+			}
+
+			evaluate(val)
+		}
+	}
+
+	if !foundMetric {
+		log.Println("metric", prmMetric, "not found in endpoint output")
+		os.Exit(2)
+	}
+}
+
+func evaluate(val float64) {
+	log.Printf("%s = %f\n", prmMetric, val)
+
+	if whiteflag.CheckInt("lt") && val < float64(whiteflag.GetInt("lt")) {
+		os.Exit(2)
+	}
+
+	if whiteflag.CheckInt("gt") && val > float64(whiteflag.GetInt("gt")) {
+		os.Exit(2)
+	}
+
+	if whiteflag.CheckInt("eq") && val == float64(whiteflag.GetInt("eq")) {
+		os.Exit(2)
+	}
+
+	if whiteflag.CheckInt("ne") && val != float64(whiteflag.GetInt("ne")) {
+		os.Exit(2)
+	}
+
+	os.Exit(0)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/DENICeG/sensu-metric-alert
 
 go 1.14
+
+require github.com/danielb42/whiteflag v0.0.0-20200602191841-abf0898b8787
